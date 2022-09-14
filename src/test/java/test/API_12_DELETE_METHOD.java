2fa923a399eae2c901efd54d7074464203fae0be
unknown <cuma.ozturkmen@gmail.com>
2022-09-14 17:04:02 +0200

    12 api güncelleme

--- src/test/java/test/API_12_DELETE_METHOD.java
+++ src/test/java/test/API_12_DELETE_METHOD.java
@@ -1,6 +1,15 @@
 package test;
 
 import baseUrl.BaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class API_12_DELETE_METHOD extends BaseUrl {
     /*
@@ -12,5 +21,24 @@ Response Code: 200
 Response Message: Account deleted!
      */
 
+    @Test
+    public void test01() {
+spec.pathParams("1","api","2","deleteAccount");
 
+        Map<String, String> data =new HashMap<>();
+
+        data.put("email", "mahmut@gmail.com");
+        data.put("password", "1234");
+
+        Response response=given().spec(spec).formParams(data).when().delete("/{1}/{2}");
+
+        response.jsonPath().prettyPrint();
+
+        response.then().assertThat().statusCode(200);
+
+        JsonPath actuelData=response.jsonPath();
+        Assert.assertEquals("Account deleted!",actuelData.getString("message"));
+        Assert.assertEquals("200", actuelData.getString("responseCode"));
+
+    }
 }
--- src/test/java/test/API_9_DELETE_To_Verify_Login.java
+++ src/test/java/test/API_9_DELETE_To_Verify_Login.java
@@ -1,6 +1,12 @@
 package test;
 
 import baseUrl.BaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class API_9_DELETE_To_Verify_Login extends BaseUrl {
     /*
@@ -10,4 +16,25 @@ Request Method: DELETE
 Response Code: 405
 Response Message: This request method is not supported.
      */
+
+    @Test
+    public void test01() {
+
+        spec.pathParams("first", "api", "second", "verifyLogin");
+
+        Response response = given().spec(spec).when().delete("/{first}/{second}");
+
+        response.jsonPath().prettyPrint();
+
+        response.then().assertThat().statusCode(200);
+
+        JsonPath actualData = response.jsonPath();
+
+        Assert.assertEquals("This request method is not supported.", actualData.getString("message"));
+        Assert.assertEquals("405", actualData.getString("responseCode"));
+
+
+
+    }
+
 }
